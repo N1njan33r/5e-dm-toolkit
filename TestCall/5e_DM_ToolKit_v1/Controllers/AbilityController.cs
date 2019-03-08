2c65cca047941a49f6b7bdbe95736facf60726d5
swurst6@gmail.com <swurst6@gmail.com>
2019-03-07 19:08:49 -0500

    Character

---
+++ TestCall/5e_DM_ToolKit_v1/Controllers/AbilityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5e_DM_ToolKit_v1.Controllers
+{
+    public class AbilityController : Controller
+    {
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Select()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestCall/5e_DM_ToolKit_v1/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5e_DM_ToolKit_v1.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
