71671f4f70c6812d41f40066c6771c5a24fbfcb5
swurst6@gmail.com <swurst6@gmail.com>
2019-03-07 20:05:22 -0500

    Database

---
+++ TestCall/5e_DM_ToolKit_v1/DAL/DungeonContext.cs
@@ -0,0 +1,22 @@
+﻿using _5e_DM_ToolKit_v1.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace _5e_DM_ToolKit_v1.DAL
+{
+    public class DungeonContext : DbContext
+    { 
+        public DungeonContext() : base("DungeonContext")
+        {
+
+        }
+
+        public DbSet<Characters> Characters { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
