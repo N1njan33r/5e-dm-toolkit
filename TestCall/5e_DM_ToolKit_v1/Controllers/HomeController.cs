38b16a111fa4488b15637ff90a0a22be473388fc
Alexander Inman <fujiwara06@gmail.com>
2019-03-10 00:41:11 -0500

    Intermittent to pull and merge

--- TestCall/5e_DM_ToolKit_v1/Controllers/HomeController.cs
+++ TestCall/5e_DM_ToolKit_v1/Controllers/HomeController.cs
@@ -1,30 +1,35 @@
-﻿using System;
+﻿using _5e_DM_ToolKit_v1.DAL;
+using _5e_DM_ToolKit_v1.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace _5e_DM_ToolKit_v1.Controllers
 {
     public class HomeController : Controller
     {
+        //private DungeonContext db = new DungeonContext();
+        List<EncounterCreature> encounterCreature = new List<EncounterCreature>();
+
         public ActionResult Index()
         {
-            return View();
+            if (encounterCreature == null)
+            {
+                return HttpNotFound();
+            }
+
+            encounterCreature.Add(new EncounterCreature { Name = "Boozy" });
+            EncounterCreature creature1 = new EncounterCreature { Name = "Booshy" };
+
+            return View("Index", creature1.Name);
         }
 
-        public ActionResult About()
+        public ActionResult Add()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        public List<EncounterCreature> encounterCreatures = new List<EncounterCreature>();
     }
 }
