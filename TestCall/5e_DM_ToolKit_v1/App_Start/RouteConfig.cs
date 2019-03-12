bfebf9085873339f726af9a823422293b68000d8
terri <thorpetann@gmail.com>
2019-03-11 21:21:08 -0400

    corrected route

--- TestCall/5e_DM_ToolKit_v1/App_Start/RouteConfig.cs
+++ TestCall/5e_DM_ToolKit_v1/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace TeamAlpha.GoldenOracle
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "TeamAlpha.GoldenOracle.Controllers"}
             );
         }
     }
