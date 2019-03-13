c7c8ac0db66d814d051dac2222ca18fe3054c4c6
terri <thorpetann@gmail.com>
2019-03-13 16:44:47 -0400

    slider

---
+++ TestCall/5e_DM_ToolKit_v1/Controllers/SliderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamAlpha.GoldenOracle.Controllers
+{
+    public class SliderController : Controller
+    {
+        // GET: Slider
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestCall/5e_DM_ToolKit_v1/Models/Slider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamAlpha.GoldenOracle.Models
+{
+    public class Slider
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int HitPoints { get; set; }
+
+    }
+}
